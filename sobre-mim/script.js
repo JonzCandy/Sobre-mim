1636c3978aa6b5c0f30e5e3cc83f908ea6af8548
JonzCandy <joncandy2021@gmail.com>
2021-11-19 10:20:05 -0300

    ta ai

---
+++ sobre-mim/script.js
@@ -0,0 +1,37 @@
+// Header
+document.getElementById("topo-page").innerHTML = "Meu Maior Hobby";
+
+//Main-LETRA A
+document.getElementById("cabeca").innerHTML = "Esse sou eu";
+document.getElementById("nome").innerHTML = "Nome: João Cândido Costa";
+document.getElementById("idade").innerHTML = "Idade: 18 anos | 27/04";
+document.getElementById("localizacao").innerHTML = "Nascido, Criado e Vivido em Salvador";
+
+let perfil = document.getElementById("essesoueu");
+perfil.src = "imgs/perfil.jpg";
+perfil.alt = "Foto minha";
+
+//Main-LETRA B
+document.getElementById("maiorhobby").innerHTML = "JOGAR RPG BORA!"
+
+let descrpg = document.getElementById("oquee");
+descrpg.href = "https://www.significados.com.br/rpg/";
+descrpg.innerHTML = "RPG, pra mim é uma arte, mas sinceramente, RPG é a sigla em inglês para role-playing game, um gênero de jogo no qual os jogadores assumem o papel de personagens imaginários, em um mundo fictício.";
+descrpg.target = "_blank";
+
+let image1 = document.getElementById("hobby1");
+image1.src = "imgs/rpg1.png";
+image1.alt = "referencia a rpg";
+let image2 = document.getElementById("hobby2");
+image2.src = "imgs/rpg2.png";
+image2.alt = "referencia a rpg";
+let image3 = document.getElementById("hobby3");
+image3.src = "imgs/rpg3.png";
+image3.alt = "referencia a rpg";
+let image4 = document.getElementById("hobby4");
+image4.src = "imgs/rpg4.png";
+image4.alt = "referencia a rpg";
+
+//Footer
+document.getElementById("quemfez").innerHTML = "Pagina feita por:";
+document.getElementById("fuieuquefiz").innerHTML = "Jonz_Candy @2021";
